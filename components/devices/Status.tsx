af433aaca1b4a3b15d4f90abc6a58d6e85f339fc
Tyler Emanuel <tyleremanuel23@gmail.com>
2022-01-27 15:12:47 -0600

    Initialized App

---
+++ components/devices/Status.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Status.module.css"
+
+const Status = () => {
+  return (
+      <div className = "title-text">Tech Tracker Site</div>
+  )
+};
+
+export default Status;
