863ba592780466e950d47e1cb886d8762e880a82
Tyler Emanuel <tyleremanuel23@gmail.com>
2022-01-31 21:49:02 -0600

    Added Dot Badges

--- components/devices/Status.tsx
+++ components/devices/Status.tsx
@@ -1,33 +1,35 @@
 import React, { useState } from 'react';
 import { Device } from '@prisma/client';
-import styles from './Status.module.css';
-import { ActionIcon, CloseButton, Input, Badge, Paper, useMantineTheme } from '@mantine/core';
+import {
+  ActionIcon, Input, Badge, Paper,
+} from '@mantine/core';
 import { Adjustments } from 'tabler-icons-react';
-
+import styles from './Status.module.css';
 
 type StatusProps = {
   device: Device;
 };
 
-
 function Status({ device }: StatusProps) {
-  const color = device.status === "ONLINE" ? 'green': 'red';
+  const color = device.status === 'ONLINE' ? 'green' : 'red';
   const [Edit, toggleEdit] = useState<Boolean>(true);
-
 
   return (
     <div className={styles['title-text']}>
       <Paper shadow="xs" radius="xl" p="md" withBorder>
         <Badge color={color} size="xl" variant="dot">{device.name}</Badge>
-        <ActionIcon onClick = {()=>toggleEdit(!Edit)}>
+        <ActionIcon onClick={() => toggleEdit(!Edit)}>
           <Adjustments size={16} />
         </ActionIcon>
         <Input variant="default" placeholder="Enter ID" disabled={!Edit} />
         <Input variant="default" placeholder="Enter IP" disabled={!Edit} />
-        <div>Last Updated: {device.lastUpdated}</div>
+        <div>
+          Last Updated:
+          {device.lastUpdated}
+        </div>
       </Paper>
     </div>
-    
+
   );
 }
 
