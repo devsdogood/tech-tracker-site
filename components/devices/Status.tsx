2582e97bc7aacedfe3b1a0977d37a61600bf855c
Tyler Emanuel <tyleremanuel23@gmail.com>
2022-01-31 21:49:02 -0600

    Added Dot Badges

--- components/devices/Status.tsx
+++ components/devices/Status.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import styles from "./Status.module.css"
+import { Badge } from '@mantine/core';
+
+
+
 
 const Status = () => {
   return (
+      <body className = {styles['main']}>
       <div className={styles['title-text']}>Tech Tracker Site</div>
+      <h1 className={styles['devices']}><Badge border-size = {100  } radius ={100} variant="dot" color = "red" size = "xl" className={styles['badges']}>Device 1</Badge></h1>
+      <h1 className={styles['devices']}><Badge radius ={100} variant="dot" color = "green" size = "xl" className={styles['badges']}>Device 2</Badge></h1>
+      </body>
+      
   )
 };
 
