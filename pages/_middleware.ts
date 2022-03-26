61d2d26d0d2414d9bbdac2d3c118ef4f56cdcdd1
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-03-26 16:28:52 -0500

    Add pages middleware

---
+++ pages/_middleware.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+const dev = process.env.NODE_ENV !== "production";
+const server = dev
+  ? "http://localhost:3000"
+  : "https://tech-tracker-site.vercel.app";
+
+export async function middleware(req: NextApiRequest) {
+  // Allow NextAuth requests
+  if (req.url!.includes("/auth/")) {
+    return NextResponse.next();
+  }
+
+  const token = await getToken({ req });
+  if (!token) return NextResponse.redirect(`${server}/api/auth/signin`);
+
+  return NextResponse.next();
+}
