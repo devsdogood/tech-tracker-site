ef8380a94b5458f8a2355de1720a3f215e3171d2
Tyler Emanuel <tyleremanuel23@gmail.com>
2022-05-03 10:56:41 -0500

    Added New Device Form

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,17 @@
 import useDevices from '@util/hooks/useDevices';
 import Status from 'components/devices/Status';
+import DeviceForm from 'components/devices/DeviceForm';
 
 function Dashboard() {
   const devices = useDevices();
 
-  return <div>{devices?.map((d) => <Status device={d} key={d.id} />)}</div>;
+  return (
+    <div>
+      {devices?.map((d) => <Status device={d} key={d.id} />)}
+      {' '}
+      <DeviceForm />
+    </div>
+  );
 }
 
 export default Dashboard;
