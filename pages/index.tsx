436fc3b1b0ca377cb6915d737674bed51eec4cad
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-10-01 23:05:18 -0500

    Use next.js `Head` to add titles to pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import { Box, SimpleGrid } from '@chakra-ui/react';
 import useDevices from '@util/hooks/useDevices';
 import Status from 'components/devices/Status';
+import Head from 'next/head';
 
 function Dashboard() {
   const devices = useDevices();
 
   return (
     <Box m="2em">
+      <Head>
+        <title>Device Dashboard</title>
+      </Head>
       <SimpleGrid minChildWidth="250px" spacing="40px">
         {devices?.map((d) => <Status device={d} key={d.id} />)}
       </SimpleGrid>
--- pages/new.tsx
+++ pages/new.tsx
@@ -1,9 +1,13 @@
 import { Box, Center } from '@chakra-ui/react';
 import DeviceForm from 'components/devices/DeviceForm';
+import Head from 'next/head';
 
 function NewDevicePage() {
   return (
     <Center width="100%" mt="2em">
+      <Head>
+        <title>Add Device</title>
+      </Head>
       <Box w="100%" maxW="500px">
         <DeviceForm buttonText="Add Device" disabled={false} />
       </Box>
